81039c63dde7c689db10c8c1dbf8886ce0969461
Sharath Kumar <70848472+Sharathkumar0@users.noreply.github.com>
2025-06-04 09:55:57 +0530

    Add files via upload

---
+++ Kafka_Flink.py
@@ -0,0 +1,50 @@
+import os
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import StreamTableEnvironment, EnvironmentSettings
+
+os.environ['FLINK_HOME'] = r'C:\flink-2.0.0'
+
+env = StreamExecutionEnvironment.get_execution_environment()
+settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+table_env = StreamTableEnvironment.create(env, environment_settings=settings)
+
+
+# Kafka source table
+table_env.execute_sql("""
+    CREATE TABLE input_table (
+        title STRING,
+        price FLOAT,
+        currency STRING,
+        url STRING       
+    ) WITH (
+        'connector' = 'kafka',
+        'topic' = 'input_topic',
+        'properties.bootstrap.servers' = 'localhost:9092',
+        'format' = 'json',
+        'properties.group.id' = 'flink-group-2',
+        'scan.startup.mode' = 'earliest-offset' 
+    )
+""")
+
+# Define Kafka sink table
+table_env.execute_sql("""
+    CREATE TABLE output_table (
+        title STRING,
+        price FLOAT,
+        currency STRING,
+        url STRING
+    ) WITH (
+        'connector' = 'kafka',
+        'topic' = 'output_topic',
+        'properties.bootstrap.servers' = 'localhost:9092',
+        'format' = 'json'
+    )
+""")
+
+# Insert data from source to sink
+table_env.execute_sql("""
+    INSERT INTO output_table
+    SELECT *
+    FROM input_table
+    WHERE price > 500
+""").wait()
